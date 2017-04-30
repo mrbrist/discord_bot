9c50301247cd9d7219e56af4886c910abf4f0f18
Joshua Weston <joshuaweston@Joshuas-MBP.fritz.box>
2017-04-30 17:03:18 +0200

    update

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,7 +1,11 @@
+const moment = require('moment');
+require("moment-duration-format");
 exports.run = (client, msg) => {
-  var x = client.uptime;
-  var Time = moment.duration(x);
-  msg.channel.sendMessage(`\`${Time.hours() + Time.minutes() + Time.seconds()}\``)
+  var A = client.uptime;
+  var seconds=(A/1000)%60
+  var minutes=(A/(1000*60))%60
+  var hours=(A/(1000*60*60))%24
+  msg.channel.sendMessage(`\`${Math.round(hours)}h ${Math.round(minutes)}m ${Math.round(seconds)}s\``)
 };
 
 exports.conf = {
