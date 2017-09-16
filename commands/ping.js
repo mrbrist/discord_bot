77f672447c901c816d96d3e53d3f6fb70c0eac49
mrbristcs <mrbristcs@gmail.com>
2017-09-17 01:43:47 +0200

    yes daddy

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,5 @@
 exports.run = (client, message) => {
-  message.channel.sendMessage('Ping?')
+  message.channel.sendMessage('Yes Daddy!')
     .then(msg => {
       msg.edit(`Pong! (took: \`${msg.createdTimestamp - message.createdTimestamp}ms\`)`);
     });
