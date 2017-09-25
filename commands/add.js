5f57823d22e333191b075312fbdd4c8ee3734c37
mrbristcs <mrbristcs@gmail.com>
2017-09-25 19:42:48 +0200

    started log event

--- commands/add.js
+++ commands/add.js
@@ -1,13 +1,13 @@
 const yt = require("ytdl-core");
 
 exports.run = (client, msg, [song]) => {
-  if (!song) return msg.channel.sendMessage("Invalid YouTube Link");
+  if (!song) return msg.channel.send("Invalid YouTube Link");
   if(!client.hasOwnProperty("queue")) client.queue = {};
   yt.getInfo(song, (err, info) => {
-    if(err) return msg.channel.sendMessage("Invalid YouTube Link: " + err);
+    if(err) return msg.channel.send("Invalid YouTube Link: " + err);
     if (!client.queue.hasOwnProperty(msg.guild.id)) client.queue[msg.guild.id] = {}, client.queue[msg.guild.id].playing = false, client.queue[msg.guild.id].songs = [];
     client.queue[msg.guild.id].songs.push({url: song, title: info.title, requester: msg.author.username});
-    msg.channel.sendMessage(`:musical_note: Added **${info.title}** to the queue :notes:`);
+    msg.channel.send(`:musical_note: Added **${info.title}** to the queue :notes:`);
   });
 };
 
--- events/message.js
+++ events/message.js
@@ -1,7 +1,21 @@
 const settings = require('../settings.json');
 module.exports = message => {
-  let client = message.client;
+
+  let logChannel = message.guild.channels.find('name', 'logchannel');
+
   if (message.author.bot) return;
+  if (message.channel.id == 361888311196909578);
+
+
+  //TODO: Fix This
+  if (message.attachments.array.length !== 0) {
+    logChannel.send(`(${message.createdTimestamp}) (TTS:${message.tts}) ${message.author.tag}@${message.channel.name}: ${message.content}`)
+  } else {
+    console.log(JSON.stringify(message.attachments.array))
+    logChannel.send(`(${message.createdTimestamp}) (TTS:${message.tts}) ${message.author.tag}@${message.channel.name}: ${JSON.stringify(message.attachments.array)} ${message.content}`)
+  }
+
+  let client = message.client;
   if (!message.content.startsWith(settings.prefix)) return;
   let command = message.content.split(' ')[0].slice(settings.prefix.length);
   let params = message.content.split(' ').slice(1);
