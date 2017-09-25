1e16881b62940a4a2ebc09b20e6aac23cd199799
mrbristcs <mrbristcs@gmail.com>
2017-09-25 16:49:10 +0200

    fixed .hat

--- commands/hat.js
+++ commands/hat.js
@@ -1,13 +1,11 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
 
-var max = message.mentions.users.size;
-var UserNum = Math.random() * (max - 2) + 2;
-let User = args.slice(UserNum).join(' ');
+var user = message.mentions.users.random();
 
   message.channel.sendMessage('Picking a name...')
   .then(msg => {
-      message.channel.sendMessage('I choose: ' + User);
+      message.channel.sendMessage('I choose: ' + user);
   });
 };
 
