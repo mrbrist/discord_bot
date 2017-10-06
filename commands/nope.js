ab5c03958daca1ed13fd86a0932f54da4ea4ad32
Rhys <rhysalexander1999@gmail.com>
2017-10-06 09:47:42 +0100

    .nope command
    Continued working on .nope command

--- commands/nope.js
+++ commands/nope.js
@@ -1,16 +1,27 @@
-var parseString = require('xml2js').parseString;
-const fetch = require('node-fetch');
+const GoogleImages = require('google-images');
+const client = new GoogleImages('010006338513942582612:hshddmo5_0w', 'AIzaSyCFbfJ_oDSfcUulPj9Qzavjk6Xb-ZOy-1c');
+
+
 exports.run = (client, message) => {
   message.channel.sendMessage('You want a nope? Really?')
     .then(msg => {
-      fetch('https://www.google.co.uk/search?q=spider/get?format=xml&results_per_page=1').then(function(res) {
-        return res.text();
-      }).then(function(body) {
-        var xml = body;
-        var url1 = xml.search("<url>");
-        var url2 = xml.search("</url>");
-        var res = xml.substring(url1 + 5, url2);
-        msg.edit(res);
+      client.search('Spider')
+      .then(images => {
+        
+          /*
+          [{
+              "url": "http://steveangello.com/boss.jpg",
+              "type": "image/jpeg",
+              "width": 1024,
+              "height": 768,
+              "size": 102451,
+              "thumbnail": {
+                  "url": "http://steveangello.com/thumbnail.jpg",
+                  "width": 512,
+                  "height": 512
+              }
+          }]
+           */
       });
     });
 };
