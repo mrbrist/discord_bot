fa428b169bdd873d4a8cb95031b68777d602015f
Turner605 <32025166+Turner605@users.noreply.github.com>
2017-10-07 23:03:02 +0100

    .musichelp
    removed .time from listing

--- commands/musichelp.js
+++ commands/musichelp.js
@@ -1,6 +1,6 @@
 const settings = require('../settings.json');
 exports.run = (client, msg) => {
-  let tosend = [settings.prefix + "join : \"Join Voice channel of msg sender\"",	settings.prefix + "add : \"Add a valid youtube link to the queue\"", settings.prefix + "queue : \"Shows the current queue, up to 15 songs shown.\"", settings.prefix + "play : \"Play the music queue if already joined to a voice channel\"", "", "the following commands only function while the play command is running:".toUpperCase(), settings.prefix + "pause : \"pauses the music\"",	settings.prefix + "resume : \"resumes the music\"", settings.prefix + "skip : \"skips the playing song\"", settings.prefix + "time : \"Shows the playtime of the song.\""];
+  let tosend = [settings.prefix + "join : \"Join Voice channel of msg sender\"",	settings.prefix + "add : \"Add a valid youtube link to the queue\"", settings.prefix + "queue : \"Shows the current queue, up to 15 songs shown.\"", settings.prefix + "play : \"Play the music queue if already joined to a voice channel\"", "", "the following commands only function while the play command is running:".toUpperCase(), settings.prefix + "pause : \"pauses the music\"",	settings.prefix + "resume : \"resumes the music\"", settings.prefix + "skip : \"skips the playing song\""];
   msg.channel.sendCode("xl",tosend);
 };
 
