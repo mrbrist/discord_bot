590704e81439512902c8d12f7aa7cdb1d16a5d81
mrbristcs <mrbristcs@gmail.com>
2017-10-07 17:36:58 +0200

    Google Images suck

--- commands/nope.js
+++ commands/nope.js
@@ -1,8 +1,12 @@
 const GoogleImages = require('google-images');
 const imagesGoogle = new GoogleImages('009565028834274790724:v44gvc8ozku', 'AIzaSyAkyJYTuOIKw42msRRuxarK8n0fPg_dQR4');
 
+function getRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 exports.run = (client, message) => {
-  imagesGoogle.search('Spider')
+  imagesGoogle.search('Spider', {page: "4"})
     .then(images => {
       message.channel.send('You want a nope? Really?')
         .then(msg => {
