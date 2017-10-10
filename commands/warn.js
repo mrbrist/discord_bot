c393aecb17cc6ed27a177694a2f5abb4fa755711
Rhys <rhysalexander1999@gmail.com>
2017-10-10 15:33:43 +0100

    .warn
    Began adding code to allow for auto demotion.

--- commands/warn.js
+++ commands/warn.js
@@ -3,9 +3,38 @@ exports.run = (client, message, args) => {
   let reason = args.slice(1).join(' ');
   let user = message.mentions.users.first();
   let modlog = message.guild.channels.find('name', 'mod-log');
+  let UserMessageCount = 0
   if (!modlog) return message.reply('I could not find the mod-log channel');
   if (reason.length < 1) return message.reply('You have not given a reason');
   if (message.mentions.users.size < 1) return message.reply('You must mention someone to warn them').catch(console.error);
+
+
+//   console.log(user.discriminator)
+// for(var i = 0; i < 9; i++){
+//     const Message = await modlog.fetchMessage(i);
+//     console.log(Message.content.includes(user.discriminator))
+//   }
+
+//TODO user search with amount of warns
+console.log("Stage 1")
+  message.channel.fetchMessages()
+  .then((messages) => {
+    console.log("Stage 2")
+    if (user) {
+      console.log("Stage 3")
+      //messages = messages.filter(user.discriminator)
+      //const filterBy = user ? user.id : Client.user.id;
+      //messages = messages.filter(m => m.author.id === filterBy).array().slice(0, messages.size);
+    let WarnAmount = messages.size
+    console.log(WarnAmount)
+    console.log("Stage 4")
+    }
+   });
+
+
+
+
+
 
   message.channel.send(`Warn successful! Logged to ${modlog}`);
   let UserRole = message.member.displayHexColor;
