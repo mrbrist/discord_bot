b05e27134cd561425aa5278505dd7a84e15342c4
Rhys <rhysalexander1999@gmail.com>
2017-10-10 16:35:31 +0100

    Channel Clearing / auto demote
    Changed .smokebomb to .purge and altered the auto demote code.

--- commands/warn.js
+++ commands/warn.js
@@ -4,37 +4,22 @@ exports.run = (client, message, args) => {
   let user = message.mentions.users.first();
   let modlog = message.guild.channels.find('name', 'mod-log');
   let UserMessageCount = 0
+  let WarnAmount = 0
+  let NewMessages = []
+
   if (!modlog) return message.reply('I could not find the mod-log channel');
   if (reason.length < 1) return message.reply('You have not given a reason');
   if (message.mentions.users.size < 1) return message.reply('You must mention someone to warn them').catch(console.error);
 
-
-//   console.log(user.discriminator)
-// for(var i = 0; i < 9; i++){
-//     const Message = await modlog.fetchMessage(i);
-//     console.log(Message.content.includes(user.discriminator))
-//   }
-
 //TODO user search with amount of warns
-console.log("Stage 1")
-  message.channel.fetchMessages()
-  .then((messages) => {
-    console.log("Stage 2")
-    if (user) {
-      console.log("Stage 3")
-      //messages = messages.filter(user.discriminator)
-      //const filterBy = user ? user.id : Client.user.id;
-      //messages = messages.filter(m => m.author.id === filterBy).array().slice(0, messages.size);
-    let WarnAmount = messages.size
-    console.log(WarnAmount)
-    console.log("Stage 4")
-    }
+  modlog.fetchMessages()
+  .then((msgs) => {
+    const filterBy = user ? user.id : Client.user.id;
+    NewMessages = msgs.filter(m => m.author.id === filterBy).array().slice(0, msg.size);
+    WarnAmount = NewMessages.size
    });
-
-
-
-
-
+   console.log(WarnAmount)
+   console.log("Stage 4")
 
   message.channel.send(`Warn successful! Logged to ${modlog}`);
   let UserRole = message.member.displayHexColor;
@@ -43,8 +28,9 @@ console.log("Stage 1")
     .setAuthor('Warning', user.avatarURL)
     .setColor(UserRole)
     .setTimestamp()
-    .addField('Reason', `${reason}`)
-    .addField('Warned By', `${message.author.username}#${message.author.discriminator}`);
+    .addField('User', `${user}`)
+    .addField('Warned By', `${message.author}#${message.author.discriminator}`)
+    .addField('Reason', `${reason}`);
     return client.channels.get(modlog.id).sendEmbed(embed);
   };
 
