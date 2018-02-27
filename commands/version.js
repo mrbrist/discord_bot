ab17e1d9c57eeb5a8b67b27c07c42fa001eeb4a2
Rhys Turner <rhysalexander1999@gmail.com>
2018-02-27 15:37:33 +0000

    Changed .version message
    added new option in .settings for devrank

--- commands/version.js
+++ commands/version.js
@@ -2,14 +2,15 @@ const version = require('../package.json').version;
 const settings = require('../settings.json');
 exports.run = (client, message) => {
   console.log(message.author.id);
-  if (message.author.id == settings.ownerid) {
+
+  if (message.member.roles.find("name", settings.devrank)){
     message.channel.sendMessage('Loading Version...')
       .then(msg => {
         msg.edit(`\`v${version}\``);
       });
     return;
   }
-  message.reply('You\'re mom gay lol');
+  message.reply('You\'re not a bot developer you don\'t need this...');
 };
 
 exports.conf = {
